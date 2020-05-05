38ff6b9282dc120f5822403e1d39ab6473bfda93
Bruce Graham <Bruce_00000@live.co.uk>
2020-05-05 11:04:11 +0000

    broken update...tried to abstract the Ticker lookup process and portfolio calculation

--- get_my_tickers.R
+++ get_my_tickers.R
@@ -1,16 +1,29 @@
 
 library(tidyquant)
-tickers<-c("KOMP","VJPN.L","GBPUSD=X")
-getSymbols(tickers,from='2018-01-01', to=TODAY())
+tickers<-c("KOMP","VJPN.L")
+startDt <- '2016-01-01'
 
-colnames(KOMP) <- c("open","high","low","close","volume","adjusted")
-colnames(VJPN.L) <- c("open","high","low","close","volume","adjusted")
-colnames(`GBPUSD=X`) <- c("open","high","low","close","volume","adjusted")
+GBPxUSD <- getSymbols("GBPUSD=X",auto.assign=FALSE,from=startDt,to=TODAY())
+
+ticky <- function(x){
+  getSymbols(x,auto.assign=FALSE,from=startDt,to=TODAY())
+}
+
+chgColNames <- function(x){
+  colnames()
+}
+
+quoteDfList <- lapply(tickers,ticky)  
+ <- for(i in 1:length(quoteDfList)){
+  colnames(quoteDfList[[i]]) <- c("open","high","low","close","volume","adjusted")
+}
+
+i <- quoteDfList[[2]]
 
 # Conversion of GBP funds to USD
-vjpnxusd <- merge.xts(VJPN.L,`GBPUSD=X`,join = "inner")
-vjpn.usd <- cbind(vjpnxusd$close * vjpnxusd$close.1)
+vjpn.merge <- merge(quoteDfList[[2]],quoteDfList[[3]],join = "inner")
+vjpn.usd <- cbind(vjpn.merge$close * vjpn.merge$close.1)
 
 # Create a portfolio ts
-all <- merge(KOMP$close,vjpn.usd, join = "outer")
+all <- merge(quoteDfList[[1]],vjpn.usd, join = "outer")
 portfolio.usd <- cbind(na.omit(all$komp.close)*1950+na.omit(all$vjpn.close*450))
