75024dba810761205d8bc2f4206db49b7e299443
mojojojoe <bruce.graham@gmail.com>
2020-10-05 05:23:41 +0200

    tried to abstractify the naming of intermediary variables

--- src/get_my_tickers.R
+++ src/get_my_tickers.R
@@ -2,6 +2,7 @@
 # returns a list of xts objects based on a list of #
 # Ticker strings, and between startDt and Today()  #
 ####################################################
+install.packages("tidyquant")
 library(tidyquant)
 
 tickers<-readxl::read_xls("/home/rstudio/portfolio/resources/KOMP_All_Holdings.xls",sheet='KOMP_All_Holdings',range = "B4:B55")
@@ -10,8 +11,7 @@ tickers<-as.list(tickers)
 
 # ticker.symbols<-lapply(tickers,as.symbol)
 
-startDt <- '2018-10-01'
-
+startDt <- as.Date("01-01-2008")
 get.price.list <- function(x){
   getSymbols(x,auto.assign=FALSE,from=startDt,to=TODAY())
 }
--- src/prepareDf.R
+++ src/prepareDf.R
@@ -4,22 +4,23 @@ source("../src/get_my_tickers.R")
 
 library(tsbox)
 
-lists.price.adjusted <- lapply(price.lists,function(x){return(x[,6])})
-lapply(lists.price.adjusted,plot)
+list.price.col6 <- lapply(price.lists,function(x){return(x[,6])})
+lapply(lists.price.col6,plot)
 
 # mergePriceTablesInList(priceDfList)
 # merge all xts obj from a list of xts objects
 # this is the same as the Reduce function with f=ts_c, x=lpa
+out = NULL
 unify.list <- function(lst){
   out <- lst[[1]]
   for(ts in seq(2,length(lst))){
-    out<-tsbox::ts_c(out,lst[[ts]])
+    out<-cbind(out,lst[[ts]])
   }
   out
 }
 
 
 
-multi.xts<-unify.list(lists.price.adjusted)
-colnames(multi.xts)<-tickers[[1]]
+multi.xts<-unify.list(lists.price.col6)
+colnames(multi.xts)<-tickers
 plot(multi.xts)
